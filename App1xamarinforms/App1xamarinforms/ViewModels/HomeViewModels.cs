5df2503324719af6233b3245bcb9821bea7078f3
= <=>
2021-02-26 10:45:26 -0400

    Las ultimas modificaciones del proyecto el cual esta listo para ser avaluado

--- App1xamarinforms/App1xamarinforms/ViewModels/HomeViewModels.cs
+++ App1xamarinforms/App1xamarinforms/ViewModels/HomeViewModels.cs
@@ -8,10 +8,34 @@ namespace App1xamarinforms.ViewModels
     public class HomeViewModels
     {
         public MenuOption  BarAndHotelsMenuOption { get; set; } = new MenuOption();
+        public MenuOption FineDining { get; set; } = new MenuOption();
+
+        public MenuOption Cafes { get; set; } = new MenuOption();
+
+        public MenuOption Nearby { get; set; } = new MenuOption();
+
+        public MenuOption FastFood { get; set; } = new MenuOption();
+
+        public MenuOption FeaturedFoods { get; set; } = new MenuOption();
         public HomeViewModels()
         {
             BarAndHotelsMenuOption.Title = "Bar and Hotels";
             BarAndHotelsMenuOption.Image = "beer";
+
+            FineDining.Title = "Fine Dining";
+            FineDining.Image = "serving";
+
+            Cafes.Title = "Cafes";
+            Cafes.Image = "cafe";
+
+            Nearby.Title = "Nearby";
+            Nearby.Image = "nearby";
+
+            FastFood.Title = "Fast Food";
+            FastFood.Image = "fastfood";
+
+            FeaturedFoods.Image = "pizzaslice";
+            FeaturedFoods.Title = "Feactured Foods";
         }
     }
 }
